2e03c51d93e8a1dec76ad0a418ed271ab7908fb1
Animesh Pandey <animeshpandey2301@gmail.com>
2026-02-09 01:27:41 +0530

    initial commit

---
+++ Module-Practice/index.js
@@ -0,0 +1,10 @@
+// const sum=require('./calc');
+
+// console.log(sum(5,10));
+
+// import {sum,multiply} from './calc.js';         
+// console.log(sum(5,10));
+// console.log(multiply(5,10));    
+
+import chalk from 'chalk';  
+console.log(chalk.blue('Hello World')); 
